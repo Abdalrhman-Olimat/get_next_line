9805f72cff395ba3569237f608e5f03e1a1d0743
Abdalrhman Olimat <abdalrhmanolimat911@gmail.com>
2024-10-16 02:21:17 +0300

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aeleimat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 12:38:10 by aeleimat          #+#    #+#             */
+/*   Updated: 2024/09/22 12:38:15 by aeleimat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(char *str);
+char	*check_for_newline(char *str);
+char	*get_next_line(int fd);
+
+#endif
